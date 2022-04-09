e6971add91a11903f1e33aead160d3fe8b214b94
Noahloq <99563001+Noahloq@users.noreply.github.com>
2022-04-09 13:04:49 -0700

    Add files via upload

--- TowerDefense.py
+++ TowerDefense.py
@@ -6,6 +6,12 @@ pygame.init()
 
 clock = time.Clock()
 
+SLOW_SPEED = 1
+
+SPAWN_RATE = 360
+FRAME_RATE = 60
+REG_SPEED = 2
+
 WINDOW_WIDTH = 1100
 WINDOW_HEIGHT = 600
 
@@ -34,8 +40,8 @@ VAMPIRE_PIZZA = transform.scale(pizza_surf, (WIDTH, HEIGHT))
 
 class VampireSprite(sprite.Sprite):
     def __init__(self):
-        super().__init__() 
-        self.speed = 2
+        super().__init__()
+        self.speed = REG_SPEED
         self.lane = randint(0, 4)
         all_vampires.add(self)
         self.image = VAMPIRE_PIZZA.copy()
@@ -46,6 +52,12 @@ class VampireSprite(sprite.Sprite):
         game_window.blit(BACKGROUND, (self.rect.x, self.rect.y), self.rect)
         self.rect.x -= self.speed
         game_window.blit(self.image, (self.rect.x, self.rect.y))
+class Counters(object):
+    def __init__(self, pizza_bucks, buck_rate, buck_booster):
+        self.loop_count = 0
+        self.display_font = font.Font('pizza_font.ttf', 25)
+        self.pizza_bucks = pizza_bucks
+        self.buck_rate = buck_rate
 
 class BackgroundTile(sprite.Sprite):
    def  __init__(self, rect):
@@ -94,6 +106,25 @@ while game_running:
         VampireSprite()
 
     for vampire in all_vampires:
+        tile_row = tile_grid[vampire.rect.y // 100]
+        vamp_left_side = vampire.rect.x // 100
+        vamp_right_side = (vampire.rect.x + vampire.recft.width) // 100
+        if 0 <= vamp_left_side <= 10:
+            left_tile = tile_row[vamp_left_side]
+        else:
+            left_tile = None
+
+        if bool(left_tile) and left_tile.effect:
+            vampire.speed = SLOW_SPEED
+        if bool(right_tile) and right_tile.effect:
+            if right_tile != left_tile:
+                vampire.speed = SLOW_SPEED
+                if vampire.rect.x <= 0:
+                    vampire.kill()
+
+
+
+        
         vampire.update(GAME_WINDOW)
 
     display.update()
